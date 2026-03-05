7319884a414211aab9f84b447cded54fcae37e15
Bel-94 <ntinyaribelinda@gmail.com>
2026-03-05 12:57:29 +0300

    feat: hide db credentials

--- local/init.sql
+++ local/init.sql
@@ -1,1 +1,6 @@
 --“first boot script” that tells Postgres:“When you create this database for the first time, create the items table automatically.”
+CREATE TABLE IF NOT EXISTS items (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
